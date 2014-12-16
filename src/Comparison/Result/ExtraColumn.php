55cb76213e4db2a697cdedd7c905ea5e33fcdf32
Benedikt Forchhammer <b.forchhammer@gmail.com>
2014-12-16 15:27:35 +0100

    Refactored some common functionality into abstract base classes.

--- src/Comparison/Result/ExtraColumn.php
+++ src/Comparison/Result/ExtraColumn.php
@@ -1,34 +1,14 @@
 <?php
 /**
  * @file
- * Contains Drupal\schema\Comparison\ExtraColumn.
+ * Contains Drupal\schema\Comparison\Result\ExtraColumn.
  */
 
 namespace Drupal\schema\Comparison\Result;
 
 
-class ExtraColumn {
-
-  protected $column_name;
-  protected $table_name;
-  protected $schema;
-
-  function __construct($table_name, $column_name, $schema) {
-    $this->table_name = $table_name;
-    $this->column_name = $column_name;
-    $this->schema = $schema;
-  }
-
-  public function getTableName() {
-    return $this->table_name;
-  }
-
-  public function getColumnName() {
-    return $this->column_name;
-  }
-
+class ExtraColumn extends BaseColumn {
   public function getSchema() {
     return $this->schema;
   }
-
 }
